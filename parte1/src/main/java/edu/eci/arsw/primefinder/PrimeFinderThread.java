43d13390235a727cb90a42e864267c430e8b85cd
Nat15005 <111907712+Nat15005@users.noreply.github.com>
2024-08-22 09:48:50 -0500

    update part1

--- parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -1,46 +1,69 @@
 package edu.eci.arsw.primefinder;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class PrimeFinderThread extends Thread{
+public class PrimeFinderThread extends Thread {
 
-	
-	int a,b;
-	
-	private List<Integer> primes=new LinkedList<Integer>();
-	
+	private final int a, b;
+	private final List<Integer> primesFound;
+	private boolean paused = false;
+	private final Lock lock = new ReentrantLock();
+
 	public PrimeFinderThread(int a, int b) {
-		super();
 		this.a = a;
 		this.b = b;
+		this.primesFound = new ArrayList<>();
 	}
 
-	public void run(){
-		for (int i=a;i<=b;i++){						
-			if (isPrime(i)){
-				primes.add(i);
-				System.out.println(i);
+	@Override
+	public void run() {
+		for (int i = a; i <= b; i++) {
+			lock.lock();
+			try {
+				synchronized (this) {
+					while (paused) {
+						wait();
+					}
+				}
+				if (isPrime(i)) {
+					primesFound.add(i);
+					System.out.println(i);
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} finally {
+				lock.unlock();
 			}
 		}
-		
-		
-	}
-	
-	boolean isPrime(int n) {
-	    if (n%2==0) return false;
-	    for(int i=3;i*i<=n;i+=2) {
-	        if(n%i==0)
-	            return false;
-	    }
-	    return true;
 	}
 
-	public List<Integer> getPrimes() {
-		return primes;
+	public void pauseThread() {
+		synchronized (this) {
+			paused = true;
+		}
 	}
-	
-	
-	
-	
+
+	public void resumeThread() {
+		synchronized (this) {
+			paused = false;
+			notify();
+		}
+	}
+
+	public List<Integer> getPrimesFound() {
+		return primesFound;
+	}
+
+	private boolean isPrime(int n) {
+		if (n <= 1) return false;
+		if (n == 2 || n == 3) return true;
+		if (n % 2 == 0 || n % 3 == 0) return false;
+		for (int i = 5; i * i <= n; i += 6) {
+			if (n % i == 0 || n % (i + 2) == 0) return false;
+		}
+		return true;
+	}
 }
